d6fdd055e2a58f3a8c51174cb32b75aa2e699c8c
Fanfan <fanfa.dong@camdora.me>
2020-01-14 16:31:15 +0800

    添加标签flowlayout

---
+++ DFBaseDemocOC/DFBaseDemocOC/Classes/Function/View/CollectionCard/DFLabelLayout.m
@@ -0,0 +1,84 @@
+//
+//  DFLabelLayout.m
+//  DFLabelTag
+//
+//  Created by user on 11/2/18.
+//  Copyright © 2018年 Fanfan. All rights reserved.
+//
+
+#import "DFLabelLayout.h"
+
+
+@interface DFLabelLayout()
+@property (nonatomic, strong) NSMutableArray *itemAttributes;
+@end
+@implementation DFLabelLayout
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.scrollDirection = UICollectionViewScrollDirectionVertical;
+        self.minimumInteritemSpacing = 10;
+        self.minimumLineSpacing = 10;
+        self.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10);
+    }
+    return self;
+}
+
+- (void)prepareLayout
+{
+    [super prepareLayout];
+    
+    NSInteger itemCount = [[self collectionView] numberOfItemsInSection:0];
+    self.itemAttributes = [NSMutableArray arrayWithCapacity:itemCount];
+    
+    CGFloat xOffset = self.sectionInset.left;
+    CGFloat yOffset = self.sectionInset.top;
+    CGFloat xNextOffset = self.sectionInset.left;
+    
+    for (NSInteger idx = 0; idx < itemCount; idx++) {
+        NSIndexPath *indexPath = [NSIndexPath indexPathForItem:idx inSection:0];
+        CGSize itemSize = [self.delegate collectionView:self.collectionView layout:self sizeForItemAtIndexPath:indexPath];
+        
+        xNextOffset+=(self.minimumInteritemSpacing + itemSize.width);
+        if (xNextOffset > [self collectionView].bounds.size.width - self.sectionInset.right) {
+            xOffset = self.sectionInset.left;
+            xNextOffset = (self.sectionInset.left + self.minimumInteritemSpacing + itemSize.width);
+            yOffset += (itemSize.height + self.minimumLineSpacing);
+        }
+        else
+        {
+            xOffset = xNextOffset - (self.minimumInteritemSpacing + itemSize.width);
+        }
+        
+        UICollectionViewLayoutAttributes *layoutAttributes =
+        [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+        
+        layoutAttributes.frame = CGRectMake(xOffset, yOffset, itemSize.width, itemSize.height);
+        [_itemAttributes addObject:layoutAttributes];
+    }
+    //    self.yOffSet = yOffset;
+//    self.YCallBack(yOffset + 50);
+    
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return (self.itemAttributes)[indexPath.item];
+}
+
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
+{
+    return [self.itemAttributes filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(UICollectionViewLayoutAttributes *evaluatedObject, NSDictionary *bindings) {
+        return CGRectIntersectsRect(rect, [evaluatedObject frame]);
+    }]];
+}
+
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds
+{
+    return NO;
+}
+
+
+
+@end
